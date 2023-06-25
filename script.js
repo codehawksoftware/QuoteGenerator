e81fbc0695a1f8cea5f80a98d36af40601cf98a4
Code Hawk <brucemc66@tpg.com.au>
2023-06-25 23:02:48 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote()
+{
+// Pick a random quote from apiQuotes
+const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+// Check if author feild is blank and replace with
+
+if (!quote.author)
+{
+    authorText.textContent = "Author Unknown"
+
+}else
+{
+
+    authorText.textContent = quote.author;
+
+}
+ // Check quote lenght to determin styling
+ if (quote.text.length > 80)
+ {
+ 
+        quoteText.classList.add('long-quote');
+
+ } else
+ {
+ 
+     quoteText.classList.remove('long-quote');
+
+ }
+
+quoteText.textContent = quote.text;
+//console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() 
+{
+    
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+    try{    
+
+        const response  = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        //console.log(apiQuotes[13]);
+        newQuote();
+    } catch (error)
+    {
+        // catch error code here
+
+    }
+
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+
+// On Load
+getQuotes();
+
+// if using local quotes from quotes file quotes.js use 
+
+    
